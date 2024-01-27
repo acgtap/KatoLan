a227cb057cbb8bd4d008ac729e3c337d1b7df357
Yang Zeng <40010177+acgtap@users.noreply.github.com>
2024-01-27 19:52:08 +0800

    Update head.ts

--- docs/.vitepress/configs/head.ts
+++ docs/.vitepress/configs/head.ts
@@ -3,6 +3,7 @@ import type { HeadConfig } from 'vitepress'
 const isDevelopment = process.env.NODE_ENV === 'development'
 
 export const head: HeadConfig[] = [
+  ['meta', { name: 'keywords', content: '虚拟局域网,异地组网,免费联机工具,远程联机,局域网联机,游戏联机,MC联机'}],
   ['meta', { name: 'theme-color', content: '#3eaf7c' }],
   ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
   ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
