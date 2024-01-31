e9b484ecbdca9b5ba10ace404e3e07c26924491c
Yang Zeng <40010177+acgtap@users.noreply.github.com>
2024-01-31 21:11:47 +0800

    修改图片与描述

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -8,26 +8,11 @@ export const nav: DefaultTheme.Config['nav'] = [
     text: '快速使用',
     items: [
       { text: '如何快速联机', link: '/fe/javascript/types' },
-      { text: '房间管理', link: '/fe/es6/' },
-      { text: '知识科普', link: '/fe/typescript/base' },
     ],
     activeMatch: '^/fe',
   },
-  {
-    text: '游戏支持与教程',
-    items: [
-      {
-        text: '常用游戏/方法',
-        items: [
-          { text: 'Minecraft', link: '/workflow/' },
-          { text: '群星', link: '/workflow/' },
-          { text: '文明6', link: '/workflow/' },
-        ],
-      },
-    ],
-    activeMatch: '^/workflow',
-  },
+  { text: '游戏支持与教程', link: '/nav', activeMatch: '^/nav' },
   { text: '常见问题', link: '/q/index', activeMatch: '^/q' },
   { text: '赞助支持我们', link: 'https://afdian.net/a/seeyouagain' },
-  { text: '我们的愿景', link: '/q/hope', activeMatch: '^/q' },
+  { text: '我们的愿景', link: '/q/hope', activeMatch: '^/q/hope' },
 ]
--- docs/nav.md
+++ docs/nav.md
@@ -1,14 +1,16 @@
 ---
-description: 前端导航是由茂茂制作并收录了大量高质量前端相关站点，为前端开发工程师提供最简单便捷的网址导航服务
+description: 收录了我们支持的一些游戏
 layoutClass: m-nav-layout
 outline: [2, 3, 4]
 ---
-
 <script setup>
 import { NAV_DATA } from './nav/data'
 </script>
+
 <style src="./nav/index.scss"></style>
 
-# 前端导航
+# 支持的游戏
 
-<MNavLinks v-for="{title, items} in NAV_DATA" :title="title" :items="items"/>
+以下不完全包含所有支持游戏，理论上支持局域网联机的游戏都可以使用此软件远程联机。
+
+`<MNavLinks v-for="{title, items} in NAV_DATA" :title="title" :items="items"/>`
