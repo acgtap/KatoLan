9d8210d96effde3b3e86fc3ef5b6feea2079137d
Yang Zeng <40010177+acgtap@users.noreply.github.com>
2025-03-12 16:22:22 +0800

    fix(auth): handle token expiration in API requests

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -4,17 +4,9 @@ export const nav: DefaultTheme.Config['nav'] = [
   { text: '首页', link: 'https://lan.katomegumi.net', activeMatch: '^/' },
   { text: '文档首页', link: '/', activeMatch: '^/' },
   { text: '马上下载', link: 'https://lan.katomegumi.net/download.html', activeMatch: '^/download' },
-  {
-    text: '快速使用',
-    items: [
-      { text: '群星快速联机', link: 'https://www.bilibili.com/video/BV1wK42117ns/?spm_id_from=333.999.0.0' },
-      {text: 'Minecraft快速联机', link: 'https://www.bilibili.com/video/BV1qZ421z7ap/?spm_id_from=333.999.0.0' },
-    ],
-    activeMatch: '^/bilibili',
-  },
-  { text: '游戏支持与教程', link: '/nav', activeMatch: '^/nav' },
   { text: '常见问题', link: '/q/index', activeMatch: '^/q/index' },
-  { text: '官群交流', link: 'https://qm.qq.com/q/jhzFDyBPz4'},
-  { text: '赞助支持我们', link: 'https://afdian.net/a/seeyouagain' },
-  { text: '我们的愿景', link: '/q/hope', activeMatch: '^/q/hope' },
+  {
+    text: '官群交流',
+    link: 'http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=I_-arUNUYkqURts3vwRmAT44H2O9EQoA&authKey=w19mK3S6ftOLQjiLnxjOiOkTP0uHSk9c33zqrj9b%2BliDev0H42O%2B7WZCROCftet5&noverify=0&group_code=741616947'
+  }
 ]
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -52,8 +52,7 @@ export default {
        * https://github.com/vuejs/vitepress/blob/main/src/client/theme-default/Layout.vue
        */
       'nav-bar-title-after': () => h(MNavVisitor),
-      'doc-after': () => h(MDocFooter),
-      'aside-bottom': () => h(MAsideSponsors)
+      'doc-after': () => h(MDocFooter)
     })
   },
   enhanceApp({ app, router }: EnhanceAppContext) {
