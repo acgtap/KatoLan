08119e6ef4977a13bd179ea143ff5e486ca2f884
Yang Zeng <40010177+acgtap@users.noreply.github.com>
2026-04-10 16:16:21 +0800

    fix: 更多教程

--- docs/.vitepress/configs/sidebar.ts
+++ docs/.vitepress/configs/sidebar.ts
@@ -1,6 +1,17 @@
 import type { DefaultTheme } from 'vitepress'
 
 export const sidebar: DefaultTheme.Config['sidebar'] = {
+  '/help/': [
+    {
+      text: '使用教程',
+      collapsed: false,
+      items: [
+        { text: '教程首页', link: '/help/index' },
+        { text: '文明 6 联机教程', link: '/help/civilization6' },
+        { text: '我的世界联机教程', link: '/help/minecraft' },
+      ],
+    },
+  ],
   '/fe/': [
     {
       text: 'JavaScript 基础知识',
@@ -10,24 +21,24 @@ export const sidebar: DefaultTheme.Config['sidebar'] = {
         { text: '引用类型的拷贝', link: '/fe/javascript/clone' },
         { text: '类型转换', link: '/fe/javascript/conversions' },
         { text: '原型和原型链', link: '/fe/javascript/prototype' },
-        { text: '继承', link: '/fe/javascript/inherit' }
-      ]
+        { text: '继承', link: '/fe/javascript/inherit' },
+      ],
     },
     {
       text: 'ES6 常用知识点',
-      link: '/fe/es6/'
+      link: '/fe/es6/',
     },
     {
       text: 'TypeScript',
-      link: '/fe/typescript/base'
+      link: '/fe/typescript/base',
     },
     {
       text: 'HTML / CSS',
       collapsed: false,
       items: [
         { text: 'HTML 理论知识点', link: '/fe/html/' },
-        { text: 'CSS 理论知识点', link: '/fe/css/' }
-      ]
+        { text: 'CSS 理论知识点', link: '/fe/css/' },
+      ],
     },
     {
       text: '浏览器与网络',
@@ -35,21 +46,21 @@ export const sidebar: DefaultTheme.Config['sidebar'] = {
       items: [
         { text: '浏览器相关知识点', link: '/fe/browser/' },
         { text: 'TCP', link: '/fe/network/tcp' },
-        { text: 'HTTP', link: '/fe/network/http' }
-      ]
+        { text: 'HTTP', link: '/fe/network/http' },
+      ],
     },
     {
       text: '概念知识点',
       collapsed: false,
       items: [
         { text: '模块化', link: '/fe/concept/module' },
-        { text: '前端页面渲染方式', link: '/fe/concept/page-rendering' }
-      ]
+        { text: '前端页面渲染方式', link: '/fe/concept/page-rendering' },
+      ],
     },
     {
       text: '编程题',
-      link: '/fe/coding/'
-    }
+      link: '/fe/coding/',
+    },
   ],
   '/analysis/': [
     {
@@ -57,9 +68,9 @@ export const sidebar: DefaultTheme.Config['sidebar'] = {
       // collapsed: false,
       items: [
         { text: 'only-allow', link: '/analysis/utils/only-allow' },
-        { text: 'clsx', link: '/analysis/utils/clsx' }
-      ]
-    }
+        { text: 'clsx', link: '/analysis/utils/clsx' },
+      ],
+    },
   ],
   '/workflow/': [
     {
@@ -68,8 +79,8 @@ export const sidebar: DefaultTheme.Config['sidebar'] = {
       items: [
         { text: '工具库整理', link: '/workflow/utils/library' },
         { text: '常用正则整理', link: '/workflow/utils/regexp' },
-        { text: '常用方法整理', link: '/workflow/utils/function' }
-      ]
+        { text: '常用方法整理', link: '/workflow/utils/function' },
+      ],
     },
     {
       text: 'CSS 相关',
@@ -77,17 +88,17 @@ export const sidebar: DefaultTheme.Config['sidebar'] = {
       items: [
         { text: 'CSS 语法', link: '/workflow/css/spec' },
         { text: 'CSS 奇淫技巧', link: '/workflow/css/tricks' },
-        { text: 'Sass 常用技巧', link: '/workflow/sass/' }
-      ]
+        { text: 'Sass 常用技巧', link: '/workflow/sass/' },
+      ],
     },
     {
       text: 'Vue 相关',
-      link: '/workflow/vue/'
+      link: '/workflow/vue/',
     },
     {
       text: 'Node 相关',
       // collapsed: false,
-      items: [{ text: 'npm 常用命令', link: '/workflow/node/npm' }]
+      items: [{ text: 'npm 常用命令', link: '/workflow/node/npm' }],
     },
     {
       text: '终端相关',
@@ -95,17 +106,17 @@ export const sidebar: DefaultTheme.Config['sidebar'] = {
       items: [
         { text: 'Zsh 配置', link: '/workflow/terminal/zsh' },
         { text: '命令行工具', link: '/workflow/terminal/toolkit' },
-        { text: 'Shell 命令', link: '/workflow/terminal/shell' }
-      ]
+        { text: 'Shell 命令', link: '/workflow/terminal/shell' },
+      ],
     },
     {
       text: 'Git 相关',
       collapsed: false,
       items: [
         { text: 'Git 相关技巧', link: '/workflow/git/' },
-        { text: 'Git 命令清单', link: '/workflow/git/command' }
-      ]
-    }
+        { text: 'Git 命令清单', link: '/workflow/git/command' },
+      ],
+    },
   ],
   '/efficiency/': [
     {
@@ -117,11 +128,11 @@ export const sidebar: DefaultTheme.Config['sidebar'] = {
         { text: 'Windows 平台', link: '/efficiency/software/windows' },
         { text: '浏览器设置与扩展', link: '/efficiency/software/browser' },
         { text: 'Visual Studio Code 配置', link: '/efficiency/software/vscode' },
-        { text: 'WebStorm 配置', link: '/efficiency/software/webstorm' }
-      ]
+        { text: 'WebStorm 配置', link: '/efficiency/software/webstorm' },
+      ],
     },
     { text: '在线工具', link: '/efficiency/online-tools' },
-    { text: '书签脚本', link: '/efficiency/bookmark-scripts' }
+    { text: '书签脚本', link: '/efficiency/bookmark-scripts' },
   ],
   '/pit/': [
     {
@@ -130,8 +141,8 @@ export const sidebar: DefaultTheme.Config['sidebar'] = {
       items: [
         { text: 'npm 踩坑记录', link: '/pit/npm' },
         { text: 'PC 踩坑记录', link: '/pit/pc' },
-        { text: 'H5 踩坑记录', link: '/pit/h5' }
-      ]
-    }
-  ]
+        { text: 'H5 踩坑记录', link: '/pit/h5' },
+      ],
+    },
+  ],
 }
